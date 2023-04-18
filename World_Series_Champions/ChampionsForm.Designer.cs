e846d847ef8c2e0cff2154d2f9c966bba0404315
Daniel Parks <adam.parks123@outlook.com>
2023-04-17 22:32:47 -0400

    Add project files.

---
+++ World_Series_Champions/ChampionsForm.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace World_Series_Champions
+{
+    partial class frmChampions
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbxTeams = new System.Windows.Forms.ListBox();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.lblResults = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbxTeams
+            // 
+            this.lbxTeams.FormattingEnabled = true;
+            this.lbxTeams.Location = new System.Drawing.Point(30, 25);
+            this.lbxTeams.Name = "lbxTeams";
+            this.lbxTeams.Size = new System.Drawing.Size(218, 251);
+            this.lbxTeams.TabIndex = 0;
+            this.lbxTeams.SelectedIndexChanged += new System.EventHandler(this.lbxTeams_SelectedIndexChanged_1);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(86, 337);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(106, 30);
+            this.btnClose.TabIndex = 1;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // lblResults
+            // 
+            this.lblResults.AutoSize = true;
+            this.lblResults.Location = new System.Drawing.Point(68, 304);
+            this.lblResults.Name = "lblResults";
+            this.lblResults.Size = new System.Drawing.Size(0, 13);
+            this.lblResults.TabIndex = 2;
+            // 
+            // frmChampions
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 388);
+            this.Controls.Add(this.lblResults);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.lbxTeams);
+            this.Name = "frmChampions";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "World Series Champions";
+            this.Load += new System.EventHandler(this.frmChampions_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbxTeams;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Label lblResults;
+    }
+}
+
